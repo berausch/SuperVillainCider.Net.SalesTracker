d2c27ed4ab1e3a8cf4ec635f74dea70cb8b716bc
Brittany Rausch and Steve Burton <brittany+steve@>
2017-02-23 11:57:24 -0800

    add crud for item, start sales crud

---
+++ src/SalesTracker/Controllers/SalesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalesTracker.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SalesTracker.Models
+{
+    public class SalesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Name");
+            return View();
+        }
+    }
+}
