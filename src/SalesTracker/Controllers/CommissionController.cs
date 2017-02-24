15cf5ce1a0c59eb55238b8b4dd13e75cad0936ac
Brittany Rausch and Steve Burton <brittany+steve@>
2017-02-23 16:34:12 -0800

    add sales and commissions

---
+++ src/SalesTracker/Controllers/CommissionController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using SalesTracker.Models;
+using System.Diagnostics;
+using System.Security.Claims;
+
+
+
+namespace SalesTracker.Controllers
+{
+    public class CommissionController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CommissionController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            var listCommission = _db.Sales.Where(x => x.User.Id == currentUser.Id).Include(s => s.Item).ToList();
+            decimal commissionTotal = 0;
+            
+            for (var i = 0; i < listCommission.Count; i++)
+            {
+                commissionTotal += listCommission[i].Item.Price;
+            }
+            decimal commissionTotal2 = Math.Round(commissionTotal, 2);
+            ViewBag.CommissionTotal = commissionTotal2/5;
+
+
+            return View(_db.Sales.Where(x => x.User.Id == currentUser.Id).Include(s => s.Item));
+        }
+    }
+}
--- src/SalesTracker/Controllers/SalesController.cs
+++ src/SalesTracker/Controllers/SalesController.cs
@@ -5,7 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using SalesTracker.Models;
+using System.Diagnostics;
+using System.Security.Claims;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,11 +17,38 @@ namespace SalesTracker.Models
 {
     public class SalesController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public SalesController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
         public IActionResult Index()
         {
-            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Name");
+            ViewBag.ItemId = new SelectList(_db.Items, "ItemId", "Name");
             return View();
+        }
+
+        public async Task<IActionResult> Create(int id)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            
+
+            var thisItem = _db.Items.FirstOrDefault(i => i.ItemId == id);
+            var thisQty = thisItem.Quantity;
+            thisQty -= 1;
+            thisItem.Quantity = thisQty;
+            _db.Entry(thisItem).State = EntityState.Modified;
+            _db.SaveChanges();
+
+            Sale thisSale = new Sale(id);
+            thisSale.User = currentUser;
+            _db.Sales.Add(thisSale);
+            _db.SaveChanges();
+            return Json(thisSale);
         }
     }
 }
